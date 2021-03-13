aedd124adc29cfad70508388438bfa2db84567a3
olivera <omicic@gmail.com>
2021-03-13 15:02:38 +0100

    sekcija

--- index.php
+++ index.php
@@ -6,6 +6,78 @@
         <p>Show your Profile to the world using online CV builder and Get Hired Faster</p>
         <button>About us</button>
     </div>
-
 </header>
+
+<section class="container sekcija">
+    <article>
+        <h2>01</h2>
+    </article>
+    <article>
+        <h3>about us</h3>
+        <h2><span>we can </span> help your business grow</h2>
+        <p>Get your business in 4 easy steps</p>
+    </article>
+
+</section>
+
+<!-- section class="services container">
+    <article>
+        <i class="far fa-char-bar"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil sapiente molestias vitae excepturi hic quod
+            aut ipsam veritatis perspiciatis culpa.</p>
+    </article>
+    <article>
+        <i class="far fa-char-bar"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil sapiente molestias vitae excepturi hic quod
+            aut ipsam veritatis perspiciatis culpa.</p>
+    </article>
+    <article>
+        <i class="far fa-char-bar"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil sapiente molestias vitae excepturi hic quod
+            aut ipsam veritatis perspiciatis culpa.</p>
+    </article>
+    <article>
+        <i class="far fa-char-bar"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil sapiente molestias vitae excepturi hic quod
+            aut ipsam veritatis perspiciatis culpa.</p>
+    </article>
+    <article>
+        <i class="far fa-char-bar"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil sapiente molestias vitae excepturi hic quod
+            aut ipsam veritatis perspiciatis culpa.</p>
+    </article>
+</section>
+
+<section class="team container">
+    <article>
+        <img src="img/team1.jpg" alt="">
+        <h4>Jeams Daglas</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores voluptate exercitationem, perferendis
+            quasi aliquid debitis nisi porro laborum cumque nobis.</p>
+    </article>
+    <article>
+        <img src="img/team2.jpg" alt="">
+        <h4>Jeams Daglas</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores voluptate exercitationem, perferendis
+            quasi aliquid debitis nisi porro laborum cumque nobis.</p>
+    </article>
+    <article>
+        <img src="img/team3.jpg" alt="">
+        <h4>Jeams Daglas</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores voluptate exercitationem, perferendis
+            quasi aliquid debitis nisi porro laborum cumque nobis.</p>
+    </article>
+</section>
+ -->
+
 <?php include "partials/footer.php" ?>
