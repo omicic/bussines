680dcee22e3693dc8694b46db0ba5cf27851969d
olivera <omicic@gmail.com>
2021-03-13 18:52:08 +0100

    team

--- index.php
+++ index.php
@@ -87,7 +87,51 @@
     </article>
 </section>
 
-<!--section class="team container">
+<section class="whatwedone">
+    <header class="container">
+        <article>
+            <h2>03</h2>
+        </article>
+        <article>
+            <h4>services</h4>
+            <h2><span>how to </span> provide tools that help anyone</h2>
+            <p>Get your business in 4 easy steps</p>
+            <button>About us</button>
+        </article>
+    </header>
+
+    <div class="container counter">
+        <div class="col">
+            <h2>18</h2>
+            <h4>Nominations</h4>
+            <hr>
+        </div>
+        <div class="col">
+            <h2>5</h2>
+            <h4>Awards</h4>
+            <hr>
+        </div>
+        <div class="col">
+            <h2>45</h2>
+            <h4>Agencies</h4>
+            <hr>
+        </div>
+
+    </div>
+</section>
+
+<section class="container team">
+    <article>
+        <h2>04</h2>
+    </article>
+    <article>
+        <h4>our team</h4>
+        <h2><span>we have a professional</span> tema of bussines analystst</h2>
+        <p>Get your business in 4 easy steps</p>
+    </article>
+</section>
+
+<section class="weare container">
     <article>
         <img src="img/team1.jpg" alt="">
         <h4>Jeams Daglas</h4>
@@ -107,6 +151,6 @@
             quasi aliquid debitis nisi porro laborum cumque nobis.</p>
     </article>
 </section>
- -->
+ 
 
 <?php include "partials/footer.php" ?>
