35ebe1afbfb5a3011115e21f857c2f631ab71b22
olivera <omicic@gmail.com>
2021-03-13 19:11:44 +0100

    footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,27 @@
+<footer>
+    <div class="container">
+        <div class="col">
+            <a href="index.php"><span>bussines</span><br>Marketing & cosulting</a>
+        </div>
+        <div class="col">
+            <i class="fas fa-map-marker-alt"></i>
+            <p>Main street 2, Novi Sad</p>
+            <p>Serbia</p>
+        </div>
+        <div class="col">
+            <i class="fas fa-envelope"></i>
+            <p>Main street 2, Novi Sad</p>
+            <p>Serbia</p>
+        </div>
+        <div class="col">
+            <i class="fas fa-phone"></i>
+            <p>Main street 2, Novi Sad</p>
+            <p>Serbia</p>
+        </div>
+    </div>
+
+</footer>
+
 </body>
 
 </html>
